d4c884c16c46a2a782a1d420ba9b0cee206114b4
sergio <sergioteran.zambrano@gmail.com>
2016-11-12 16:53:18 -0400

    Rating en detalles de local

--- src/pages/item-details/item-details.ts
+++ src/pages/item-details/item-details.ts
@@ -8,9 +8,18 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ItemDetailsPage {
   selectedItem: any;
+  ratings: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('local');
+  }
+
+    getNumber(numero){
+    this.ratings = [];
+    for (let i = 0 ; i <= numero -1; i++){
+    this.ratings.push(i);
+    }
+    return this.ratings;
   }
 }
