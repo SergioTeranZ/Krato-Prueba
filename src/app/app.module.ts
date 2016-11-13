e6d61d92a2f14eb5e194de0793e02f3c80db8305
sergio <sergioteran.zambrano@gmail.com>
2016-11-13 10:13:06 -0400

    UI arreglada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
+
 import { MyApp } from './app.component';
 import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
 import { ItemDetailsPage } from '../pages/item-details/item-details';
 import { ListPage } from '../pages/list/list';
 import { KratoPage } from '../pages/krato/krato';
+
 import {ComerciosService} from '../providers/comercios-service';
+
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+
+// AF2 Settings
+export const firebaseConfig = {
+    apiKey: "AIzaSyCeQdNeNM-ppKKdDfMf_Bn67fhzhi_1GQ4",
+    authDomain: "angularprueba-55b94.firebaseapp.com",
+    databaseURL: "https://angularprueba-55b94.firebaseio.com",
+    storageBucket: "angularprueba-55b94.appspot.com",
+    messagingSenderId: "120586352780"
+};
+
+// IGNORE EL AUTH
 
 @NgModule({
   declarations: [
@@ -16,7 +32,8 @@ import {ComerciosService} from '../providers/comercios-service';
     KratoPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
